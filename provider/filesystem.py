6c4b13f5f896311540f277620df4e895b96e1778
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2015-09-25 18:49:58 +0300

    Load each submission only once

--- provider/filesystem.py
+++ provider/filesystem.py
@@ -8,7 +8,7 @@ from radar.config import tokenizer_config
 def hook(request, course, config):
     """
     Ignore - there is only command line interface for file system provider.
-    
+
     """
     pass
 
@@ -16,7 +16,7 @@ def hook(request, course, config):
 def cron(course, config):
     """
     Nothing to do.
-    
+
     """
     pass
 
@@ -24,16 +24,19 @@ def cron(course, config):
 def load_submission_dir(exercise, path):
     """
     Inserts a submission from directory for a given exercise.
-    
+
     """
     if not os.path.isdir(path):
         return None
     text = files.join_files(_read_directory(path), tokenizer_config(exercise.tokenizer))
-    student = exercise.course.get_student(_safe_student_name(path))
-    submission = Submission(exercise=exercise, student=student)
-    submission.save()
-    files.put_submission_text(submission, text)
-    return submission
+    (name, n) = _safe_student_name(path)
+    student = exercise.course.get_student(name)
+    if student.submissions.filter(exercise=exercise).count() < n:
+        submission = Submission(exercise=exercise, student=student)
+        submission.save()
+        files.put_submission_text(submission, text)
+        return submission
+    return None
 
 
 def _read_directory(path):
@@ -41,11 +44,12 @@ def _read_directory(path):
     for file_name in os.listdir(path):
         file_path = os.path.join(path, file_name)
         if os.path.isfile(file_path):
-            with open(file_path, "r") as f:
-                file_map[file_name] = f.read()
+            with open(file_path, "rb") as f:
+                file_map[file_name] = f.read().decode("utf-8", "ignore")
     return file_map
 
 
 def _safe_student_name(path):
-    (name, _) = os.path.basename(path).split(".", 1)
-    return name
+    print(path)
+    (name, num) = os.path.basename(path).split(".", 1)
+    return (name, int(num))
