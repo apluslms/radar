a459becaaa41f8c935c1c1e2897599dcca445155
Matias Lindgren <matias.lindgren@aalto.fi>
2018-02-07 17:45:59 +0200

    Purge trailing whitespace

--- review/decorators.py
+++ review/decorators.py
@@ -10,11 +10,11 @@ STUDENT_KEY = "student_key"
 def access_resource(view_func):
     """
     Accesses the resource selected by named URL patterns.
-    
+
     """
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-        
+
         if COURSE_KEY not in kwargs:
             raise RuntimeError("Expected pattern \"course_name\" in the URL.")
         course = get_object_or_404(Course, key=kwargs[COURSE_KEY])
@@ -22,7 +22,7 @@ def access_resource(view_func):
 
         if EXERCISE_KEY in kwargs:
             kwargs["exercise"] = get_object_or_404(Exercise, course=course, key=kwargs[EXERCISE_KEY])
-        
+
         if STUDENT_KEY in kwargs:
             kwargs["student"] = get_object_or_404(Student, course=course, key=kwargs[STUDENT_KEY])
 
