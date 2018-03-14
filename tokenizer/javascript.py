b840fe6166bc6d98889190814d2d9db361ce18ba
Matias Lindgren <matias.lindgren@aalto.fi>
2018-03-14 20:50:56 +0200

    Rename tokenizer dir to avoid confusion with library

--- tokenizer/javascript.py
+++ tokenizer/javascript.py
@@ -10,7 +10,7 @@ def tokenize(source, config):
     Runs the JavaScript tokenizer with Node.js in a subprocess and returns the token string and token index pairs as a JSON string.
     """
     try:
-        parsed = run(("node", "tokenizer/esprima/tokenizer.js"), source)
+        parsed = run(("node", "tokenizer/javascript/tokenizer.js"), source)
         data = json.loads(parsed.decode("utf-8"))
         return data["tokens"], json.dumps(data["indexes"])
     except Exception as e:
