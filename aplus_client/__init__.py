c89d7961ae73b421e92c511f5dc0edbd02701441
Matias Lindgren <matias.lindgren@aalto.fi>
2018-06-12 10:40:18 +0300

    Replace A+ API client with pip package

--- aplus_client/__init__.py
+++
@@ -1,3 +0,0 @@
-
-# Make it possible to define only `aplus_client` in django settings.py INSTALLED_APPS
-default_app_config = 'aplus_client.django.apps.AplusClientConfig'
--- aplus_client/django/__init__.py
+++
@@ -1,6 +0,0 @@
-try:
-    import django
-except ImportError:
-    raise ImportError("aplus_client.django requires django and is only usful with it")
-
-default_app_config = 'aplus_client.django.apps.AplusClientConfig'
