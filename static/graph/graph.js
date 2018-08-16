4a490bf602cd8435212880036676200da790f5ee
Matias Lindgren <matias.lindgren@aalto.fi>
2018-08-16 17:39:02 +0300

    Move all graph implementations to single file

--- static/graph/graph.js
+++ static/graph/graph.js
@@ -1,71 +1,179 @@
-$(_ => {
+var sigmaObject;
+var sigmaFilter;
+const forceAtlasConfig = {
+  // compute algorithm using a web worker
+  worker: true,
+  // running time optimization for large graphs
+  barnesHutOptimize: true,
+  // seems to increase the repulsion between nodes
+  outboundAttractionDistribution: true
+};
+
+
+function applyMinEdgeSizeFilter(newMinEdgeSize) {
+  sigmaFilter
+    .undo('min-edge-size')
+    .edgesBy(e => e.size >= newMinEdgeSize, 'min-edge-size')
+    .apply();
+}
+
+function applyMinEdgeWeightFilter(newMinEdgeWeight) {
+  sigmaFilter
+    .undo('min-edge-weight')
+    .edgesBy(e => e.weight >= newMinEdgeWeight, 'min-edge-weight')
+    .apply();
+}
+
+function applyDisconnectedNodesFilter() {
+  sigmaFilter
+    .undo('disconnected-nodes')
+    .nodesBy(n => !sigmaObject.graph.adjacentEdges(n.id).every(e => e.hidden), 'disconnected-nodes')
+    .apply();
+}
+
+function shuffleGraphLayout(s) {
+  s.killForceAtlas2();
+  s.graph.nodes().forEach(n => {
+    n.x = Math.random() - 0.5;
+    n.y = Math.random() - 0.5;
+  });
+  s.refresh();
+}
+
+function drawGraph(graphData) {
+  let s = new sigma({
+    renderer: {
+      container: "graph-container",
+      type: 'canvas'
+    },
+    settings: {
+      minEdgeSize: 1,
+      maxEdgeSize: 10,
+      enableEdgeHovering: true,
+      defaultEdgeHoverColor: '#222',
+      edgeHoverExtremities: true,
+      edgeLabelSize: 'proportional',
+      edgeLabelSizePowRatio: 1.5,
+    }
+  });
+
+  graphData.nodes.forEach(node => {
+    s.graph.addNode({
+      id: node,
+      label: node,
+      size: 1,
+      color: '#444',
+    });
+  });
+  graphData.edges.forEach((edge, i) => {
+    const matchCount = edge.matches_in_exercises.length;
+    s.graph.addEdge({
+      id: 'e' + i,
+      source: edge.source,
+      target: edge.target,
+      size: matchCount * 10,
+      label: '' + matchCount,
+      color: '#ccc',
+      hover_color: '#222',
+      weight: matchCount,
+      matchesData: Array.from(edge.matches_in_exercises),
+    });
+  });
+
+  sigmaFilter = new sigma.plugins.filter(s);
+
+  shuffleGraphLayout(s);
+
+  return s;
+}
+
+function arrayToHTML(strings) {
+    return "<ul>" + strings.map(s => "<li>" + s + "</li>").join("\n") + "</ul>";
+}
+
+function matchToHTML(match) {
+    const elements = [
+        "Exercise: <a href='" + match.exercise_url + "'>" + match.exercise_name + "</a>",
+        "Comparison view: <a href='" + match.comparison_url + "'>link</a>",
+        "Maximum similarity: " + match.max_similarity,
+    ];
+    return arrayToHTML(elements);
+}
+
+var buildingGraph = false;
+
+function buildGraph(graphData) {
     const shuffleLayoutButton = $("#shuffle-layout-button");
     const forceAtlasButton = $("#toggle-forceatlas-button");
     const buildGraphButton = $("#build-graph-button");
     const minMatchCountSlider = $("#min-match-count-range");
     const minMatchCountSliderValue = $("#min-match-count-range-value");
+    const minSimilaritySlider = $("#min-similarity-range");
+    const minSimilaritySliderValue = $("#min-similarity-value");
     const summaryModal = $("#pair-comparisons-summary-modal");
-
-    function getSliderValue() {
-        return parseInt(minMatchCountSlider.val());
-    }
-
-    function arrayToHTML(strings) {
-        return "<ul>" + strings.map(s => "<li>" + s + "</li>").join("\n") + "</ul>";
-    }
-
-    function matchToHTML(match) {
-        const elements = [
-            "Exercise: <a href='" + match.exercise_url + "'>" + match.exercise_name + "</a>",
-            "Comparison view: <a href='" + match.comparison_url + "'>link</a>",
-            "Maximum similarity: " + match.max_similarity,
-        ];
-        return arrayToHTML(elements);
-    }
 
     function handleEdgeClick(event) {
         const edge = event.data.edge;
         summaryModal.find("h4.modal-title").text(edge.source + " and " + edge.target + " have " + edge.matchesData.length + " submission pairs with high similarity");
         summaryModal.find("div.modal-body").html(arrayToHTML(edge.matchesData.map(matchToHTML)));
         summaryModal.modal("toggle");
+        // TODO fire leave edge hover event to prevent edge highlighting from being stuck when returning from modal view
     }
 
-    const js = new JS();
-    // TODO: build graph in parallel with UI thread using a WebWorker
-    const s = js.drawGraph(js.parseJSON($("#graph-json")));
+    // Draw graph and assign resulting sigma.js object to global variable
+    sigmaObject = drawGraph(graphData);
 
-    s.bind("clickEdge", handleEdgeClick);
+    sigmaObject.bind("clickEdge", handleEdgeClick);
 
     shuffleLayoutButton.on("click", _ => {
-        shuffleGraphLayout(s);
+        shuffleGraphLayout(sigmaObject);
         if (forceAtlasButton.hasClass("active")) {
             // shuffleGraphLayout kills force atlas, fix inconsistent visual state
             forceAtlasButton.button("toggle");
         }
     });
+
     forceAtlasButton.on("click", _ => {
-        if (!s.isForceAtlas2Running()) {
-            s.startForceAtlas2(forceAtlasConfig);
+        if (!sigmaObject.isForceAtlas2Running()) {
+            sigmaObject.startForceAtlas2(forceAtlasConfig);
         } else {
-            s.stopForceAtlas2();
+            sigmaObject.stopForceAtlas2();
         }
     });
+
     buildGraphButton.on("click", _ => {
-        s.killForceAtlas2();
+        buildingGraph = true;
+        sigmaObject.killForceAtlas2();
+        // Assuming we are at graph, do POST to graph/build, with build args in body
+        const minSimilarity = minSimilaritySlider.val();
+        const minMatchCount = minMatchCountSlider.val();
+        const csrfToken = $("input[name=csrfmiddlewaretoken]").val();
+        $.ajax({
+            url: "build",
+            type: "POST",
+            data: {
+                minSimilarity: minSimilarity,
+                minMatchCount: minMatchCount,
+            },
+            dataType: "json",
+            success: buildGraph,
+            error: console.error,
+            beforeSend: xhr => xhr.setRequestHeader("X-CSRFToken", csrfToken),
+        });
     });
-    minMatchCountSlider.on("change", _ => {
-        const newMinSize = getSliderValue();
+
+    minMatchCountSlider.on("input", _ => {
+        const newMinSize = parseInt(minMatchCountSlider.val());
         minMatchCountSliderValue.text(newMinSize);
         applyMinEdgeWeightFilter(newMinSize);
         applyDisconnectedNodesFilter();
     });
 
-    s.startForceAtlas2(forceAtlasConfig);
-    let timeoutID = window.setTimeout(_ => {
-        sigmaObject.stopForceAtlas2();
-        window.clearTimeout(timeoutID);
-        [shuffleLayoutButton, forceAtlasButton].forEach(button => {
-            button.attr("disabled", false);
-        });
-    }, 2000);
-});
+    if (buildingGraph) {
+        buildingGraph = false;
+    }
+}
+
+function buildGraphFromJSON(elementID) {
+    return buildGraph(JSON.parse($("#" + elementID).text()));
+}
