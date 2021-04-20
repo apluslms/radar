ef8ba7147edf568c4de5d9dfa480db9fe5535d66
Sami Laine <sami.v.laine@aalto.fi>
2021-04-20 19:00:06 +0300

    Initial radar dockerfile

--- docker/rootfs/srv/radar-cont-settings.py
+++ docker/rootfs/srv/radar-cont-settings.py
@@ -2,29 +2,27 @@ DEBUG = True
 #SECRET_KEY = 'not a very secret key'
 ADMINS = (
 )
-#ALLOWED_HOSTS = ["*"]
+# NOTE: All other services define this in settings.py
+ALLOWED_HOSTS = ["*"]
 
-STATIC_ROOT = '/local/aplus/static/'
-MEDIA_ROOT = '/local/aplus/media/'
+STATIC_ROOT = '/local/radar/static/'
+MEDIA_ROOT = '/local/radar/media/'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aplus',
+        'NAME': 'radar',
     },
 }
+
+PROVIDERS['a+'].update({'host': 'http://plus:8000'})
 
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': '/run/aplus/django-cache',
+        'LOCATION': '/run/radar/django-cache',
     },
 }
-
-REMOTE_PAGE_HOSTS_MAP = {
-    "grader:8080": "localhost:8080",
-}
-OVERRIDE_SUBMISSION_HOST = "http://plus:8000"
 
 #CELERY_BROKER_URL = "amqp://"
 
--- docker/rootfs/srv/radar-init.sh
+++ docker/rootfs/srv/radar-init.sh
@@ -3,4 +3,4 @@
 # Start background services/tasks
 #start_services
 # start course updater (will exit when successful)
-run_services aplus-course-update aplus-lti-services
+# run_services aplus-course-update aplus-lti-services
