8246afcf65387c0a970e9e6cf74b30ccf628cb47
Sami Laine <sami.v.laine@aalto.fi>
2021-05-11 17:38:40 +0300

    Export LTI keys for A+

--- docker/rootfs/srv/radar-init.sh
+++ docker/rootfs/srv/radar-init.sh
@@ -1,6 +1,14 @@
 #!/bin/sh -eu
 
-# Start background services/tasks
-#start_services
-# start course updater (will exit when successful)
-# run_services aplus-course-update aplus-lti-services
+# Ensure LTI key
+if ! setuidgid $USER python3 manage.py list_lti_keys -k testradar 2>/dev/null | grep -qsF testradar; then
+    setuidgid $USER python3 manage.py add_lti_key -k testradar -s testradar -d "test key" 2>/dev/null
+else
+    setuidgid $USER python3 manage.py list_lti_keys -k testradar 2>/dev/null
+fi
+
+# Export LTI key for A+
+mkdir -p /data/aplus/lti-services-in/
+setuidgid $USER python3 \
+    manage.py export_for_aplus -k testradar -l Radar -i send -b http://$(hostname -i):8001 \
+    > /data/aplus/lti-services-in/radar.json
